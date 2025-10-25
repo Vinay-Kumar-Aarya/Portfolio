b420d8ec5c65d39299d2288db6d3ccf430048c41
Vinay Aarya <91550559+Vinay-Kumar-Aarya@users.noreply.github.com>
2025-10-25 17:56:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student","Web Developer", "Android Developer","Freelancer"],
+        strings: ["Freelancer","Web Developer", "Android Developer","Salesforce Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student","Web Developer", "Android Developer","Freelancer"],
+        strings: ["Freelancer","Web Developer", "Android Developer","Salesforce Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
